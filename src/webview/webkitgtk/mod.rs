5be7b27a28046a691e40d27e1fd688f030d99925
Lucas Nogueira <lucas@crabnebula.dev>
2023-09-19 12:57:44 -0300

    fix warning

--- src/webview/webkitgtk/mod.rs
+++ src/webview/webkitgtk/mod.rs
@@ -51,7 +51,7 @@ impl InnerWebView {
   pub fn new(
     window: Rc<Window>,
     mut attributes: WebViewAttributes,
-    pl_attrs: super::PlatformSpecificWebViewAttributes,
+    _pl_attrs: super::PlatformSpecificWebViewAttributes,
     web_context: Option<&mut WebContext>,
   ) -> Result<Self> {
     let window_rc = Rc::clone(&window);
